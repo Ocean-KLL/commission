bc33d28ec1adfe0dd513b308d0579e9daef99684
Rabby <rabbymbamu@gmail.com>
2022-05-25 23:38:39 +0200

    chnager le passord oublié

--- resources/views/clients/login.blade.php
+++ resources/views/clients/login.blade.php
@@ -16,7 +16,7 @@
     <!-- Login Form -->
     <form>
       <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
-      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+      <input type="text" id="password" class="oassword second" name="paasword" placeholder="password">
       <input type="submit" class="fadeIn fourth" value="Se connecter">
     </form>
     <div id="formFooter">
